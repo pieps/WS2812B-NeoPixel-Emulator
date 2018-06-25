519d61f4592c4791197b875dfd7325665b695259
pieps <nick@pieps.org>
2018-06-25 13:25:20 -0500

    Added various modes. Still working on getting red glow right for shady mode.

--- arduino_sketch.cpp
+++ arduino_sketch.cpp
@@ -8,12 +8,6 @@
 // Select number of pixels.
 #define NUM_PIXELS 32
 
-// We copy some type name defines here to keep the sketch self contained.
-#define u8 uint8_t
-#define u16 uint16_t
-#define u32 uint32_t
-#define s16 int16_t
-
 // Create the NeoPixel driver object. On the PC, this becomes the emulator.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_PIXELS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
@@ -24,17 +18,32 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_PIXELS, NEOPIXEL_PIN, NEO_GRB +
 #include <stdio.h>
 #endif
 
+enum Mode {
+  RING = 0,
+  BLINK = 1,
+  FILL_RING = 2,
+  QUAD_BLINK = 3,
+  TRACERS = 4,
+  SHADY = 5,
+  MODE_MAX = 6,
+};
+
 // Example of wrapping stuff that will only compile in the IDE.
-uint8_t  offset = 0; // Position of spinny eyes
-uint32_t hue  = 0xFF0000; // Start red
-uint8_t mode = 1;
+int8_t  offset = 0; // Position of spinny eyes
+uint32_t rgb  = 0xFF0000; // Start red
+float hue = 0.f;
+uint8_t mode = 0;
 uint32_t prevTime;
+bool color = true;
+int8_t inc = 1;
+uint8_t iterations = 0;
+bool forceMode = false;
  
 #ifdef Arduino_h
 void debugMarker()
 {
   pinMode(1, OUTPUT);
-  for (u8 i = 0; i < 10; ++i) {
+  for (uint8_t i = 0; i < 10; ++i) {
     digitalWrite(1, HIGH);
     delay(500);
     digitalWrite(1, LOW);
@@ -44,6 +53,42 @@ void debugMarker()
 #endif
 
 // End of boilerplate.
+
+void ring(uint8_t i, uint32_t t);
+void fillRing(uint8_t i, uint32_t t);
+
+void enterMode(uint32_t t) {
+}
+void exitMode(uint32_t t) {
+}
+uint32_t getHue() {
+  hue = fmod(hue + 1, 360.f);
+  return HueToRgb(hue);
+}
+
+uint32_t HueToRgb(float h, float s, float v) {
+  int c = s * v * 255;
+  int m = (v * 255) - c;
+  int x = c * (1.f - abs(fmod(h / 60.f, 2.f) - 1.f));
+  switch ((int) h / 60) {
+    case 0:
+      return ((c + m) << 16) + ((x + m) << 8) + m;
+    case 1:
+      return ((x + m) << 16) + ((c + m) << 8) + m;
+    case 2:
+      return (m << 16) + ((c + m) << 8) + x + m;
+    case 3:
+      return (m << 16) + ((x + m) << 8) + c + m;
+    case 4:
+      return ((x + m) << 16) + (m << 8) + c + m;
+    default: // 5
+      return ((c + m) << 16) + (m << 8) + x + m;
+	}
+}
+
+uint32_t HueToRgb(float h) {
+  return HueToRgb(h, 1.f, 1.f);
+}
 
 void setup() {
 // Arduino_h is only defined when compiling the sketch in the Arduino IDE.
@@ -59,41 +104,180 @@ void setup() {
 }
 
 void loop(){
-  uint8_t  i;
-  uint32_t t;
+  uint32_t t = millis();
 
+  enterMode(t);
   switch (mode) {
-    case 0:
-      ring(i, t);
+    case RING:
+      ring(t);
       break;
-    case 1:
-      fillRing(i, t);
+    case BLINK:
+      blink(t);
+      break;
+    case FILL_RING:
+      fillRing(t);
+      break;
+    case QUAD_BLINK:
+      quadBlink(t);
+      break;
+    case TRACERS:
+      tracers(t);
+      break;
+    case SHADY:
+      shady(t);
       break;
   }
+  exitMode(t);
 }
 
-void ring(uint8_t i, uint32_t t) {
-  for(i=0; i<16; i++) {
+void ring(uint32_t t) {
+  for(int8_t i=0; i<16; i++) {
     uint32_t c = 0;
-    if(((offset + i) & 7) < 2) c = hue;
-    //if(((offset + i) & 8 && (offset + i) & 7 < 2)) c = hue + 128;
-    pixels.setPixelColor(   i, c); // First eye
-    pixels.setPixelColor(31-i, c); // Second eye (flipped)
-  }
-  pixels.show();
-  offset++;
-  delay(50);
-}
+    if(((offset + i) & 7) < 2) {
 
-void fillRing(uint8_t i, uint32_t t) {
-  for(i=0; i<16; i++) {
-    uint32_t c = 0;
-    if(((offset + i) & 7) < 1) {
-      pixels.setPixelColor(   i, hue); // First eye
-      pixels.setPixelColor(31-i, hue); // Second eye (flipped)
+      pixels.setPixelColor(   i, getHue()); // First eye
+      pixels.setPixelColor(31-i, getHue()); // Second eye (flipped)
     }
   }
   pixels.show();
   offset++;
   delay(50);
+
+  if (t - prevTime > 8000) {
+    nextMode(t);
+  }
+}
+
+void fillRing(uint32_t t) {
+  for(int8_t i=0; i<16; i++) {
+    uint32_t c = 0;
+    if(((offset + i) & 7) < 1) {
+      pixels.setPixelColor(   i, color ? getHue() : 0); // First eye
+      pixels.setPixelColor(31-i, color ? getHue() : 0); // Second eye (flipped)
+    }
+  }
+  pixels.show();
+  offset++;
+  if (offset == 16) {
+    offset = 0;
+    color = !color;
+    iterations++;
+  }
+
+  delay(50);
+
+  if (iterations == 8) {
+    nextMode(t);
+  }
+}
+
+void blink(uint32_t t) {
+  if (iterations == 0) {
+    offset = 8;
+  }
+  resetFrameBuffer();
+  for(int8_t i=0; i<8; i++) {
+    if(i < offset) {
+      pixels.setPixelColor(i, getHue());
+      pixels.setPixelColor(15 - i, getHue());
+      pixels.setPixelColor(24 + i, getHue());
+      pixels.setPixelColor(23 - i, getHue());
+    }
+  }
+  pixels.show();
+  delay(50);
+  offset += inc;
+  if (offset > 8 || offset < 0) {
+    inc = -inc;
+    iterations++;
+    if (iterations == 4) {
+      delay(500);
+      nextMode(t);
+    }
+  }
+}
+
+void quadBlink(uint32_t t) {
+  resetFrameBuffer();
+  for(int8_t i=0; i<4; i++) {
+    if(i < offset) {
+      pixels.setPixelColor(3 - i, getHue());
+      pixels.setPixelColor(4 + i, getHue());
+      pixels.setPixelColor(11 - i, getHue());
+      pixels.setPixelColor(12 + i, getHue());
+
+      pixels.setPixelColor(28 + i, getHue());
+      pixels.setPixelColor(27 - i, getHue());
+      pixels.setPixelColor(20 + i, getHue());
+      pixels.setPixelColor(19 - i, getHue());
+    }
+  }
+  pixels.show();
+  delay(50);
+  offset += inc;
+  if (offset > 4 || offset < 0) {
+    inc = -inc;
+    iterations++;
+    if (iterations == 4) {
+      delay(500);
+      nextMode(t);
+    }
+  }
+}
+
+void tracers(uint32_t t) {
+  resetFrameBuffer();
+  for (uint8_t i = 0; i < 16; i++) {
+    if(((offset + i) & 7) < 2) {
+      pixels.setPixelColor(i, getHue());
+      pixels.setPixelColor(31 - i, getHue());
+    }
+  }
+  pixels.show();
+  offset++;
+  delay(50);
+  if (t - prevTime > 8000) {
+    nextMode(t);
+  }
+}
+
+void shady(uint32_t t) {
+  resetFrameBuffer();
+
+  pixels.setPixelColor(4 + (inc * 4), redScale(getHue()));
+  pixels.setPixelColor(11 + (-inc * 4), redScale(getHue()));
+  pixels.setPixelColor(20 + (inc * 4), redScale(getHue()));
+  pixels.setPixelColor(27 + (-inc * 4), redScale(getHue()));
+
+  pixels.show();
+  delay(50);
+  if (t - prevTime > 500) {
+    prevTime = t;
+    inc *= -1;
+    iterations++;
+  }
+  if (iterations == 8) {
+    nextMode(t);
+  }
+}
+
+uint32_t redScale(uint32_t hue) {
+  return (int) (hue * (1.f / 0xFFFFFF) * 0xFF) << 16;
+}
+
+void resetFrameBuffer() {
+  for (int8_t i = 0; i  < 32; i++) {
+    pixels.setPixelColor(i, 0);
+  }
+}
+
+void nextMode(uint32_t t) {
+  prevTime = t;
+  offset = 0;
+  inc = 1;
+  color = true;
+  if (!forceMode) {
+    mode = (mode + 1) % MODE_MAX;
+    iterations = 0;
+  }
 }
--- arduino_sketch.hpp
+++ arduino_sketch.hpp
@@ -1,20 +1,33 @@
 /* This file was automatically generated.  Do not edit! */
-s16 wrap(s16 val,s16 maxVal);
-u8 colorChannelClampedAdd(u8 a,u8 b);
-u32 additiveColorMix(u32 color1,u32 color2);
-u8 channelWeightedAvg(u8 a,u8 b,u8 weight);
-void colorPackedToScalar(u8 *r,u8 *g,u8 *b,u32 color);
-void addPixelColor(u16 pixelPos,u32 addColor);
-u32 colorWeightedAvg(u32 color1,u32 color2,u8 weight);
-u32 calcTaperedSegmentPixel(u8 numSegmentPixels,u8 pos,u32 centerColor);
-void drawTaperedSegment(u16 superPos,u8 numSegmentPixels,u32 color);
-u16 wrapAdd(s16 val,s16 addVal,s16 maxVal);
-void clear(u32 color,u16 clearMs);
-void smoothRunners(u16 runSec,u16 delayMs,struct Segment *segmentPtrIn,u8 numSegments);
-void xmasRedGreenTwinkles(u16 runSec);
+extern float hue;
+uint32_t redScale(uint32_t hue);
+void resetFrameBuffer();
+void nextMode(uint32_t t);
+void shady(uint32_t t);
+void tracers(uint32_t t);
+void quadBlink(uint32_t t);
+void blink(uint32_t t);
 void loop();
 void setup();
+uint32_t HueToRgb(float h,float s,float v);
+uint32_t HueToRgb(float h);
+uint32_t getHue();
+void exitMode(uint32_t t);
+void enterMode(uint32_t t);
+void fillRing(uint8_t i,uint32_t t);
+void fillRing(uint32_t t);
+void ring(uint8_t i,uint32_t t);
+void ring(uint32_t t);
 #if defined(Arduino_h)
 void debugMarker();
 #endif
+extern bool forceMode;
+extern uint8_t iterations;
+extern int8_t inc;
+extern bool color;
+extern uint32_t prevTime;
+extern uint8_t mode;
+extern float brightness;
+extern uint32_t rgb;
+extern int8_t offset;
 extern Adafruit_NeoPixel pixels;
